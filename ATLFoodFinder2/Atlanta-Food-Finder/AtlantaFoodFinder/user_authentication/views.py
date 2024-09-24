ee12a8201468c40f707c918f97c15bcfdf70a1ef
nli21 <NORAHJADELI@GMAIL.COM>
2024-09-24 15:08:17 -0400

    Home page and latest stuff

--- ATLFoodFinder2/Atlanta-Food-Finder/AtlantaFoodFinder/user_authentication/views.py
+++ ATLFoodFinder2/Atlanta-Food-Finder/AtlantaFoodFinder/user_authentication/views.py
@@ -27,7 +27,7 @@ def registerPage(request):
 
 def loginPage(request):
     if request.user.is_authenticated:
-        return redirect('login')
+        return redirect('home')
     else:
         if request.method == 'POST':
             username = request.POST.get('username')
